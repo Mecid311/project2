5dd95c8e0749bd74f25ad326cb153ca7f28ca9cc
Mecid311 <majidan@code.edu.az>
2019-09-23 12:59:14 -0700

    new2project

--- js/custom.js
+++ js/custom.js
@@ -42,3 +42,53 @@ $("#menu_closer").click(function(){
     $(this).parent().removeClass("active");
   }
 })
+//owl carusel comment
+
+if ( $('.owl-slide-3').length > 0 ) {
+  $('.owl-slide-3').owlCarousel({
+    center: false,
+    items: 1,
+    loop: true,
+    stagePadding: 0,
+    margin: 30,
+    autoplay: true,
+    smartSpeed: 500,
+    nav: true,
+    dots: true,
+    responsive:{
+      600:{
+        items: 2
+      },
+      1000:{
+        items: 2
+      },
+      1200:{
+        items: 3
+      }
+    }
+  });
+}
+siteCarousel();
+
+
+var sitePlusMinus = function() {
+  $('.js-btn-minus').on('click', function(e){
+    e.preventDefault();
+   
+    if ( $(this).closest('.input-group').find('.form-control').val() != 0 ) {
+      $(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) - 1);
+    } else {
+      $(this).closest('.input-group').find('.form-control').val(parseInt(0));
+    }
+  });
+  $('.js-btn-plus').on('click', function(e){
+    e.preventDefault();
+    if($(this).closest('.input-group').find('.form-control').val()<10){
+      $(this).closest('.input-group').find('.form-control').val(parseInt($(this).closest('.input-group').find('.form-control').val()) + 1);
+    }
+    else{
+      $(this).closest('.input-group').find('.form-control').val(parseInt(0));
+    }
+  });
+};
+sitePlusMinus();
